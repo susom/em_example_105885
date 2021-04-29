cfba8517e9a8c79ddb440bbfd276a0207bfbc5f4
Andrew Martin <andy123@stanford.edu>
2021-04-29 15:44:38 -0700

    add a second method

--- EmExample105885.php
+++ EmExample105885.php
@@ -8,8 +8,7 @@ class EmExample105885 extends \ExternalModules\AbstractExternalModule {
 
     use emLoggerTrait;
 
-    public function redcap_save_record( $project_id, $record, $instrument, $event_id, $group_id = NULL, $survey_hash = NULL, $response_id = NULL, $repeat_instance = 1) {
-
+    public function ugly_method($record,$instrument,$event_id) {
         $updateValues = False;
 
         $ts = date("Y-m-d H:i");  // Todays date to use for setting
@@ -42,6 +41,61 @@ class EmExample105885 extends \ExternalModules\AbstractExternalModule {
                 'dataFormat' => 'array',
                 'overwriteBehavior' => 'overwrite',
                 'data' => $updateData
+            ];
+
+            $q = REDCap::saveData($parameters);
+            if (!empty($q['errors'])) {
+                $this->emError("Invalid save", $q, $parameters);
+            }
+        }
+    }
+
+
+    public function redcap_save_record( $project_id, $record, $instrument, $event_id, $group_id = NULL, $survey_hash = NULL, $response_id = NULL, $repeat_instance = 1) {
+        $this->ugly_method($record,$instrument,$event_id);
+        // or maybe try this
+
+        $this->better_method($record,$instrument,$event_id);
+    }
+
+
+    /**
+     * This might be a more traditional way to do this where you configure it with parameters so it
+     * is more adaptable and can be used with other projects
+     * @param $record
+     * @param $instrument
+     * @param $event_id
+     */
+    public function better_method($record, $instrument, $event_id) {
+        $instances = $this->getSubSettings('instances');
+        foreach ($instances as $i => $instance) {
+            $logic = $instance['action-logic'];
+            $dest_event_id = empty($instance['dest-event']) ? $event_id : $instance['dest-event'];
+            $dest_field_name = $instance['dest-field'];
+
+            // Confirm Logic is Set
+            if (empty($logic)) {
+                $this->emError("Required logic is missing!");
+                continue;
+            }
+
+            // Evaluate logic:
+            $eval = REDCap::evaluateLogic($logic, $this->getProjectId(), $record, $event_id);
+            if (!$eval) {
+                $this->emDebug("$i: logic false - skip");
+                continue;
+            }
+
+            // Confirm Dest Field is Set
+            if (empty($dest_field_name)) {
+                $this->emError("Required dest field is missing!");
+                continue;
+            }
+
+            $this->emDebug("Found match with instance $i and $logic for $dest_event_id / $dest_field_name");
+            $parameters = [
+                'dataFormat' => 'array',
+                'data' => [$record => [$dest_event_id => [$dest_field_name = date("Y-m-d H:i")]]]
             ];
 
             $q = REDCap::saveData($parameters);
